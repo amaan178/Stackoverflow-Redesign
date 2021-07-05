348b2854b2d28459f1300a728acfc9c28211814a
Amaan Jambura <amaanjambura786@gmail.com>
2021-07-05 19:00:44 +0530

    Created migration for questions with its mutato

---
+++ app/Models/Question.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class Question extends Model
+{
+    use HasFactory;
+    protected $guarded = ['id'];
+
+    public function setTitleAttribute(string $title)
+    {
+        $this->attributes['title'] = $title;
+        $this->attributes['slug'] = Str::slug($title);
+    }
+
+    public function owner()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+}
