96b40a79b130d98af9a197a6f65ec7779fdfa2e5
Amaan Jambura <amaanjambura786@gmail.com>
2021-07-05 19:01:22 +0530

    added relationship method in user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -40,4 +40,10 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /* RELATIONSHIPS METHOD */
+    public function questions()
+    {
+        return $this->hasMany(Question::class);
+    }
 }
