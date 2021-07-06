16f69b14c54661b2178feddd2a26499574ac3988
Amaan Jambura <amaanjambura786@gmail.com>
2021-07-06 12:18:07 +0530

    Added refuired functions, accessors and mutators in Questions controller and model

---
+++ app/Http/Controllers/QuestionsController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\Questions\CreateQuestionRequest;
+use App\Models\Question;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class QuestionsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+
+     public function __construct()
+     {
+         $this->middleware(['auth'])->only('create','store');
+     }
+    public function index()
+    {
+        $questions = Question::with('owner')->latest()->paginate(10);
+        return view('questions.index', compact(['questions']));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        app('debugbar')->disable();
+        return view('questions.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(CreateQuestionRequest $request)
+    {
+        auth()->user()->questions()->create([
+            'title' => $request->title,
+            'body'  => $request->body
+        ]);
+
+        session()->flash('success','Question has been added successfully!');
+        return redirect(route('questions.index'));
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Question  $question
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Question $question)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Question  $question
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Question $question)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Question  $question
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Question $question)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Question  $question
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Question $question)
+    {
+        //
+    }
+}
--- app/Models/Question.php
+++ app/Models/Question.php
@@ -11,12 +11,34 @@ class Question extends Model
     use HasFactory;
     protected $guarded = ['id'];
 
+    public function getUrlAttribute()
+    {
+        return "questions/{$this->id}";
+    }
+
+    public function getCreatedDateAttribute()
+    {
+        return $this->created_at->diffForHumans();
+    }
+
+    public function getAnswerStyleAttribute()
+    {
+        if($this->answers_count > 0){
+            if($this->best_answer_id){
+                return "has-best-answer";
+            }
+            return "answered";
+        }
+        return "unanswered";
+    }
+    /*
+    *   MUTATORS
+    */
     public function setTitleAttribute(string $title)
     {
         $this->attributes['title'] = $title;
         $this->attributes['slug'] = Str::slug($title);
     }
-
     public function owner()
     {
         return $this->belongsTo(User::class, 'user_id');
