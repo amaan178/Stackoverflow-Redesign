c281e40456853b87efab3e725ca9df2026631668
Amaan Jambura <amaanjambura786@gmail.com>
2021-07-10 19:12:13 +0530

    Created Question Policy

---
+++ app/Policies/QuestionPolicy.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Question;
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class QuestionPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Create a new policy instance.
+     *
+     * @return void
+     */
+
+    public function update(User $user, Question $question)
+    {
+        return $user->id === $question->user_id;
+    }
+
+    public function delete(User $user, Question $question)
+    {
+        return $user->id === $question->user_id;
+    }
+
+    public function __construct()
+    {
+        //
+    }
+}
