e9d0c7e61ca502a891c6497e19acf6b5a1da3d50
Amaan Jambura <amaanjambura786@gmail.com>
2021-07-10 18:55:28 +0530

    added edit and delete button in index

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -1,46 +1,65 @@
-@extends('layouts.app');
-
+@extends('layouts.app')
 @section('content')
-   <div class="container">
-       <div class="row justify-content-center">
-           <div class="col-md-12">
-            <div class="d-flex justify-content-end mb-2">
-                <a href="{{ route('questions.create') }}" class="btn btn-outline-primary">Ask a Question!</a>
-            </div>
-               <div class="card">
-                   <div class="card-header">
-                       <h2>All Questions</h2></div>
-                       @foreach ($questions as $question)
-                           <div class="card-body">
-                               <div class="media">
-                                   <div class="mr-4 statistics">
-                                        <div class="votes text-center mb-3">
-                                            <strong class="d-block">{{$question->votes_count}}</strong>Votes
-                                        </div>
-                                        <div class="text-center mb-3 answers {{$question->answer_style}}">
-                                            <strong class="d-block">{{$question->answers_count}}</strong>Answers
-                                        </div>
-                                        <div class="views text-center">
-                                            <strong class="d-block">{{$question->views_count}}</strong>Views
-                                        </div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <div class="d-flex justify-content-end mb-2">
+                    <a href="{{ route('questions.create') }}" class="btn btn-outline-primary">Ask a Question!</a>
+                </div>
+                <div class="card">
+                    <div class="card-header">
+                        <h2>All Questions</h2>
+                    </div>
+                    @foreach ($questions as $question)
+                        <div class="card-body">
+                            <div class="media">
+                                <div class="mr-4 statistics">
+                                    <div class="votes text-center mb-3">
+                                        <strong class="d-block">{{ $question->votes_count }}</strong>Votes
+                                    </div>
+                                    {{--  --}}
+                                    <div class="answers text-center mb-3 {{ $question->answer_style }}">
+                                        <strong class="d-block">{{ $question->answers_count }}</strong> Answers
+                                    </div>
+                                    <div class="views text-center mb-3">
+                                        <strong class="d-block">{{ $question->views_count }}</strong> Views
+                                    </div>
+                                </div>
+                                <div class="media-body">
+                                    <div class="d-flex justify-content-between">
+                                        <h4><a href="{{ $question->url }}">{{ $question->title }}</a></h4>
+                                        <div>
+                                            @if (auth()->check() && auth()->user()->can('update-question', $question))
+                                                <a href="{{ route('questions.edit', $question->id) }}"
+                                                    class="btn btn-sm btn-outline-info">Edit</a>
 
-                                   </div>
-                                   <div class="media-body">
-                                       <h4><a href="{{ $question->url }}">{{$question->title}}</a></h4>
-                                       <p>
-                                           Asked By: <a href="#">{{$question->owner->name}}</a>
-                                           <span class="text-muted">{{$question->created_date}}</span>
-                                       </p>
-                                       <p>{!! \Illuminate\Support\Str::limit($question->body, 255) !!}</p>
-                                   </div>
-                               </div>
-                           </div>
-                       @endforeach
-                       <div class="card-footer">
-                           {{ $questions->links() }}
-                       </div>
-               </div>
-           </div>
-       </div>
-   </div>
+                                            @endif
+                                            @can('delete-question', $question)
+                                                <form action="{{ route('questions.destroy', $question->id) }}" method="POST">
+                                                    @csrf
+                                                    @method('DELETE')
+                                                    <button type="submit" class="btn btn-sm btn-outline-danger"
+                                                        onclick="return confirm('Are you sure you want to delete?')">Delete</button>
+                                                </form>
+
+                                            @endcan
+                                        </div>
+                                    </div>
+                                    <p>Asked By: <a href="#">{{ $question->owner->name }}</a>
+                                        <span class="text-muted">{{ $question->created_date }}</span>
+                                    </p>
+                                    <p>{!! Str::limit($question->body, 255, '...') !!}</p>
+                                </div>
+                            </div>
+                        </div>
+                        <hr>
+                    @endforeach
+                    <div class="card-footer">
+                        {{ $questions->links() }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
 @endsection
