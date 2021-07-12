97d7d9b5b6fa469277f0a6d964c409f8aa01f584
Amaan Jambura <amaanjambura786@gmail.com>
2021-07-12 09:55:44 +0530

    created routes for bestAnswer, to add favorites and unfavorite a question

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AnswersController;
+use App\Http\Controllers\FavoritesController;
 use App\Http\Controllers\QuestionsController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
@@ -24,3 +26,7 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::resource('questions', QuestionsController::class)->except('show');
 Route::get('questions/{slug}', [QuestionsController::class, 'show'])->name('questions.show');
+Route::resource('questions.answers', AnswersController::class)->except('index', 'show', 'create');
+Route::put('answers/{answer}/best-answer',[AnswersController::class, 'bestAnswer'])->name('answers.bestAnswer');
+Route::post('questions/{question}/favorite', [FavoritesController::class, 'store'])->name('questions.favorite');
+Route::post('questions/{question}/unfavorite', [FavoritesController::class, 'destroy'])->name('questions.unfavorite');
